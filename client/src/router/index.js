485a44010cd7a222a5f2764cb5c9a3ad1e6fc865
aurkid saha <sahaorchid722@gmail.com>
2021-12-01 18:33:41 +0530

    login signup done

---
+++ client/src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Login from '../views/auth/Login.vue'
+import Signup from '../views/auth/Signup.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
---
+++ client/src/store/index.js
@@ -0,0 +1,13 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    userData:[]
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
