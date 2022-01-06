9e3f7d8a6d0f20fbd403564154192cb10f58892e
aurkid saha <sahaorchid722@gmail.com>
2022-01-06 11:29:24 +0530

    contact page added

---
+++ client/src/views/contact/Contact.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Contact Us</h1>
+        <p>Name: Aurkid Saha</p>
+        <p>Email: aurkid@gmail.com</p>
+        <p>Phone: 1234567890</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Contact'
+}
+</script>
